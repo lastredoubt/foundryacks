dbc30d3d6f3c8f28fdb5f209f55b00fe84b51ce5
Tiago do Amaral Rodrigues <wtrmute@gmail.com>
2024-01-20 18:50:22 -0300

    Fix dragging of items to the hotbar

--- src/module/macros.js
+++ src/module/macros.js
@@ -8,28 +8,28 @@
 /**
  * Create a Macro from an Item drop.
  * Get an existing item macro if one exists, otherwise create a new one.
- * @param {Object} data     The dropped data
+ * @param {Object} context     The dropped data
  * @param {number} slot     The hotbar slot to use
  * @returns {Promise}
  */
-export async function createAcksMacro(data, slot) {
-    if ( data.type !== "Item" ) return;
-    if (!( "data" in data ) ) return ui.notifications.warn("You can only create macro buttons for owned Items");
-    const item = data.data;
-  
-    // Create the macro command
-    const command = `game.acks.rollItemMacro("${item.name}");`;
-    let macro = game.macros.find(m => (m.name === item.name) && (m.command === command));
-    if ( !macro ) {
-      macro = await Macro.create({
-        name: item.name,
-        type: "script",
-        img: item.img,
-        command: command,
-        flags: {"acks.itemMacro": true}
-      });
-    }
-    game.user.assignHotbarMacro(macro, slot);
+export function createAcksMacro(context, slot) {
+    if ( context.type !== "Item" ) return;
+    if (!( "uuid" in context ) ) return ui.notifications.warn("You can only create macro buttons for owned Items");
+    fromUuid(context.uuid).then(async item => {
+      // Create the macro command
+      const command = `game.acks.rollItemMacro("${item.name}");`;
+      let macro = game.macros.find(m => (m.name === item.name) && (m.command === command));
+      if ( !macro ) {
+        macro = await Macro.create({
+          name: item.name,
+          type: "script",
+          img: item.img,
+          command: command,
+          flags: {"acks.itemMacro": true}
+        });
+      }
+      game.user.assignHotbarMacro(macro, slot);
+    });
     return false;
   }
   
